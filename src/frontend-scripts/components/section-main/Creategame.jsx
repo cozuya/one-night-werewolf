04f4d1391929c7bead2016304f7e5833731a1dde
Chris Ozols <chris.v.ozols@gmail.com>
2015-11-29 21:36:51 -0600

    more create game stuff

--- src/frontend-scripts/components/section-main/Creategame.jsx
+++ src/frontend-scripts/components/section-main/Creategame.jsx
@@ -4,13 +4,15 @@ import React from 'react';
 import $ from 'jquery';
 import Popup from 'semantic-ui-popup';
 import Dropdown from 'semantic-ui-dropdown';
+import Progress from 'semantic-ui-progress';
 
 $.fn.dropdown = Dropdown;
 $.fn.popup = Popup;
+$.fn.progress = Progress;
 
 export default class Creategame extends React.Component {
 	componentDidMount () {
-		$(this.refs.defaultrolespopup).popup({
+		$(this.refs.defaultrolespopup).add(this.refs.kobkpopup).popup({
 			inline: true,
 			hoverable: true,
 			position: 'bottom center',
@@ -22,6 +24,15 @@ export default class Creategame extends React.Component {
 
 		$(this.refs.timedropdown).dropdown({
 			on: 'hover'
+		});
+
+		$(this.refs.progressbar).progress({
+			percent: 20,
+			total: 10,
+			label: 'ratio',
+			text: {
+				ratio: '{value} of {total}'
+			}
 		});
 	}
 
@@ -53,10 +64,13 @@ export default class Creategame extends React.Component {
 							Automatically selects 2 werewolves, 3 villagers, and one each of the seer, robber, troublemaker, insomniac, and hunter.
 						</div>
 					</div>
-					<div className="four wide column">
+					<div className="four wide column timeofgame">
 						<h4 className="ui header">Time of game</h4>
 						<div className="ui dropdown" ref="timedropdown">
-							<i className="dropdown icon"></i>
+							<span className="text">3:00</span>
+							<i className="dropdown icon">
+								<div className="menu"></div>
+							</i>
 							<div className="menu">
 								<div className="item">0:30</div>
 								<div className="item">1:00</div>
@@ -71,9 +85,20 @@ export default class Creategame extends React.Component {
 							</div>
 						</div>
 					</div>
-					<div className="four wide column">
-						4
+					<div className="four wide column killorbekilled">
+						<h4 className="ui header">Kill or be killed mode</h4>
+						<div className="ui fitted toggle checkbox" ref="kobkpopup">
+							<input type="checkbox" name="kobk" defaultChecked></input>
+							<label></label>
+						</div>
+						<div className="ui small popup top left transition hidden">
+							At least one player is a werewolf i.e. not all werewolf cards are in the center.
+						</div>
 					</div>
+				</div>
+				{/* roles go here */}
+				<div className="ui teal progress" ref="progressbar" data-value="2" data-total="10">
+					<div className="bar"></div>
 				</div>
 			</section>
 		);
