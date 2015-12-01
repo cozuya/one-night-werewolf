5fdc3c9d4d37f21dedc205bfa6a588712a0e49be
Chris Ozols <chris.v.ozols@gmail.com>
2015-11-30 20:45:22 -0600

    create game copy for popups mostly

--- src/frontend-scripts/components/section-main/Creategame.jsx
+++ src/frontend-scripts/components/section-main/Creategame.jsx
@@ -22,6 +22,16 @@ export default class Creategame extends React.Component {
 			}
 		});
 
+		$(this.refs.role_werewolf).popup({
+			inline: true,
+			hoverable: true,
+			position: 'top left',
+			delay: {
+				show: 300,
+				hide: 800
+			}
+		});
+
 		$(this.refs.timedropdown).dropdown({
 			on: 'hover'
 		});
@@ -53,7 +63,7 @@ export default class Creategame extends React.Component {
 						<div className="ui input">
 							<input placeholder="New Game"></input>
 						</div>
-						<p>*Optional</p>
+						<small>*Optional</small>
 					</div>
 					<div className="four wide column selectdefaults">
 						<h4 className="ui header">Select default roles</h4>
@@ -99,68 +109,98 @@ export default class Creategame extends React.Component {
 				<div className="ui grid five column pickroles">
 					<div className="row">
 						<div className="column werewolf">
-							<p>Werewolf</p>
+							<p ref="role_werewolf">Werewolf</p>
+							<div className="ui small popup bottom right transition hidden">
+								Werewolves wake up first, and look for other werewolves.  If there are none, they may look at a center card.  Werewolves are on the <span>werewolf team.</span>
+							</div>
 							<i className="minus icon"></i>
 							<span>2</span>
 							<i className="plus icon"></i>
 						</div>
 						<div className="column minion">
 							<p>Minion</p>
+							<div className="ui small popup bottom right transition hidden">
+								Minions wake up, and get to see who the werewolves are - but they are not aware of the minion.  Minions win if the werewolves win, and in the avent of no werewolves, win if a villager dies.  Minions are on the <span>werewolf team.</span>
+							</div>
 							<i className="minus icon"></i>
 							<span>0</span>
-							<i className="plus icon"></i>							
+							<i className="plus icon"></i>
 						</div>
 						<div className="column">
 							<p>Mason</p>
+							<div className="ui small popup bottom right transition hidden">
+								Masons wake up, and look for other masons.  Masons are on the <span>village team.</span>
+							</div>
 							<i className="minus icon"></i>
 							<span>0</span>
-							<i className="plus icon"></i>							
+							<i className="plus icon"></i>
 						</div>
 						<div className="column">
 							<p>Seer</p>
+							<div className="ui small popup bottom right transition hidden">
+								Seers wake up, and have the choice of looking at another player's card, or two of the center cards.  Seers are on the <span>village team.</span>
+							</div>
 							<i className="minus icon"></i>
 							<span>0</span>
-							<i className="plus icon"></i>							
+							<i className="plus icon"></i>
 						</div>
 						<div className="column">
 							<p>Robber</p>
+							<div className="ui small popup bottom right transition hidden">
+								Robbers wake up, and select a player's card.  They then swap that player's card with their own, and become the role and team they have stolen (and vice versa) - however they do not do an additional night action.  Robbers are on the <span>village team.</span>
+							</div>
 							<i className="minus icon"></i>
 							<span>0</span>
-							<i className="plus icon"></i>							
+							<i className="plus icon"></i>
 						</div>
 					</div>
 					<div className="row">
 						<div className="column">
 							<p>Troublemaker</p>
+							<div className="ui small popup bottom right transition hidden">
+								Troublemakers wake up, and swap the cards of two players without looking at them.  Troublemakers are on the <span>village team.</span>
+							</div>
 							<i className="minus icon"></i>
 							<span>0</span>
-							<i className="plus icon"></i>							
+							<i className="plus icon"></i>
 						</div>
 						<div className="column">
 							<p>Insomniac</p>
+							<div className="ui small popup bottom right transition hidden">
+								Insomniacs wake up, and look at their card again to see if they are still the insomniac.  Insomniacs are on the <span>village team.</span>
+							</div>
 							<i className="minus icon"></i>
 							<span>0</span>
-							<i className="plus icon"></i>							
+							<i className="plus icon"></i>
 						</div>
 						<div className="column">
 							<p>Hunter</p>
+							<div className="ui small popup bottom right transition hidden">
+								Hunters do not wake up.  If a hunter is eliminated, the player he or she is selecting for elimination is also eliminated.  Hunters are on the <span>village team.</span>
+							</div>
 							<i className="minus icon"></i>
 							<span>0</span>
-							<i className="plus icon"></i>							
+							<i className="plus icon"></i>
 						</div>
 						<div className="column tanner">
 							<p>Tanner</p>
+							<div className="ui small popup bottom right transition hidden">
+								Tanners do not wake up.  Tanners are suicidal and only win if they are eliminated.  Tanners are on <span>their own team individually</span> and do not win if another tanner wins.
+							</div>
 							<i className="minus icon"></i>
 							<span>0</span>
-							<i className="plus icon"></i>							
+							<i className="plus icon"></i>
 						</div>
 						<div className="column">
 							<p>Villager</p>
+							<div className="ui small popup bottom right transition hidden">
+								Villagers do not wake up.  Villagers are on the <span>village team.</span>
+							</div>
 							<i className="minus icon"></i>
 							<span>0</span>
-							<i className="plus icon"></i>							
+							<i className="plus icon"></i>
 						</div>
-					</div>					
+					</div>
 				</div>
 				<div>
 					<div className="ui teal progress" ref="progressbar" data-value="2" data-total="10">
