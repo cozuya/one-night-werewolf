778f089a45bd0b2c777d4b8c8bf35cdfdac8e2dc
Chris Ozols <chris.v.ozols@gmail.com>
2015-12-02 18:50:56 -0600

    count stuff mostly works, ww count does not

--- src/frontend-scripts/components/section-main/Creategame.jsx
+++ src/frontend-scripts/components/section-main/Creategame.jsx
@@ -11,7 +11,15 @@ $.fn.popup = Popup;
 $.fn.progress = Progress;
 
 export default class Creategame extends React.Component {
-	componentDidMount () {
+	constructor() {
+		this.handleChangeRole = this.handleChangeRole.bind(this);
+		// this.roleCount = this.roleCount.bind(this);
+		this.state = {
+			roles: ['werewolf', 'werewolf']
+		}
+	}
+
+	componentDidMount() {
 		$(this.refs.defaultrolespopup).add(this.refs.kobkpopup).popup({
 			inline: true,
 			hoverable: true,
@@ -22,10 +30,10 @@ export default class Creategame extends React.Component {
 			}
 		});
 
-		$(this.refs.role_werewolf).popup({
+		$(this.refs.role_werewolf).add(this.refs.role_minion).add(this.refs.role_mason).popup({
 			inline: true,
 			hoverable: true,
-			position: 'top left',
+			lastResort: false,
 			delay: {
 				show: 300,
 				hide: 800
@@ -41,9 +49,44 @@ export default class Creategame extends React.Component {
 			total: 10,
 			label: 'ratio',
 			text: {
-				ratio: '{value} of {total} roles selected'
+				ratio: '{value} of {total} roles'
 			}
 		});
+	}
+
+	handleChangeRole(el) {
+		let role = $(el.target).parent().find('p').attr('data-role'),
+			increment = $(el.target).hasClass('plus'),
+			$progress = $(this.refs.progressbar),
+			roles = this.state.roles,
+			werewolfCount = roles.filter((el) => {
+				return el === 'werewolf';
+			}).length;
+
+		console.log(werewolfCount);
+
+		if (increment) {
+			if (roles.length <= 7) {
+				roles.push(role);
+				this.setState({roles});
+				$progress.progress('increment');
+			}
+		} else {
+			if (roles.length >= 0 && werewolfCount >= 2) {
+				roles.splice(roles.indexOf(role), 1);
+				console.log(roles);
+				this.setState({roles});
+				$progress.progress('decrement');
+			}
+		}
+
+		// console.log(this.state.roles);
+	}
+
+	roleCount(role) {
+		return this.state.roles.filter((el) => {
+			return el === role;
+		}).length;
 	}
 
 	render() {
@@ -59,11 +102,10 @@ export default class Creategame extends React.Component {
 				</div>
 				<div className="ui grid">
 					<div className="four wide column gamename">
-						<h4 className="ui header">Game name</h4>
+						<h4 className="ui header">Game name<small>*Optional</small></h4>
 						<div className="ui input">
 							<input placeholder="New Game"></input>
 						</div>
-						<small>*Optional</small>
 					</div>
 					<div className="four wide column selectdefaults">
 						<h4 className="ui header">Select default roles</h4>
@@ -109,26 +151,26 @@ export default class Creategame extends React.Component {
 				<div className="ui grid five column pickroles">
 					<div className="row">
 						<div className="column werewolf">
-							<p ref="role_werewolf">Werewolf</p>
-							<div className="ui small popup top left transition hidden">
+							<p ref="role_werewolf" data-role="werewolf">Werewolf</p>
+							<div className="ui small popup transition hidden">
 								Werewolves wake up first, and look for other werewolves.  If there are none, they may look at a center card.
 								  There is a minimum of 2 werewolves in every game.  Werewolves are on the <span>werewolf team.</span>
 							</div>
-							<i className="minus icon"></i>
-							<span>2</span>
-							<i className="plus icon"></i>
+							<i className="minus icon" onClick={this.handleChangeRole}></i>
+							<span>{this.roleCount('werewolf')}</span>
+							<i className="plus icon" onClick={this.handleChangeRole}></i>
 						</div>
 						<div className="column minion">
-							<p>Minion</p>
+							<p ref="role_minion">Minion</p>
 							<div className="ui small popup bottom right transition hidden">
-								Minions wake up, and get to see who the werewolves are - but they are not aware of the minion.  Minions win if the werewolves win, and in the avent of no werewolves, win if a villager dies.  Minions are on the <span>werewolf team.</span>
+								Minions wake up, and get to see who the werewolves are - but the werewolves are not aware of who the minions are.  Minions win if the werewolves win, and in the event of no werewolves, win if a villager dies.  Minions are on the <span>werewolf team.</span>
 							</div>
 							<i className="minus icon"></i>
 							<span>0</span>
 							<i className="plus icon"></i>
 						</div>
-						<div className="column">
-							<p>Mason</p>
+						<div className="column mason">
+							<p ref="role_mason">Mason</p>
 							<div className="ui small popup bottom right transition hidden">
 								Masons wake up, and look for other masons.  Masons are on the <span>village team.</span>
 							</div>
@@ -148,7 +190,7 @@ export default class Creategame extends React.Component {
 						<div className="column">
 							<p>Robber</p>
 							<div className="ui small popup bottom right transition hidden">
-								Robbers wake up, and select a player's card.  They then swap that player's card with their own, and become the role and team they have stolen (and vice versa) - however they do not do an additional night action.  Robbers are on the <span>village team.</span>
+								Robbers wake up, and look at another player's card.  They then swap that player's card with their own, and become the role and team they have stolen (and vice versa) - however they do not do an additional night action.  Robbers are on the <span>village team.</span>
 							</div>
 							<i className="minus icon"></i>
 							<span>0</span>
@@ -203,17 +245,19 @@ export default class Creategame extends React.Component {
 						</div>
 					</div>
 				</div>
-				<div>
-					<div className="ui teal progress" ref="progressbar" data-value="2" data-total="10">
-						<div className="bar">
-							<div className="progress">
-								
+				<div className="ui grid footer">
+					<div className="twelve wide column">
+						<div className="ui teal progress" ref="progressbar" data-value="2" data-total="10">
+							<div className="bar">
+								<div className="progress"></div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div className="ui button primary disabled">
-					Create game
+					<div className="four wide column">
+						<div className="ui button primary disabled">
+							Create game
+						</div>
+					</div>
 				</div>
 			</section>
 		);
