6be289d6599535e74565ccd4bae87270f2cdb653
Chris Ozols <chris.v.ozols@gmail.com>
2015-12-19 17:08:44 -0600

    change to kobk logic that is significantly less dumb

--- routes/socket/game-internals.js
+++ routes/socket/game-internals.js
@@ -5,31 +5,30 @@ import _ from 'lodash';
 
 export function startGame(game) {  // this fires twice which really can't happen so need to be fixed first
 	let roles = game.roles,
+		allWerewolvesNotInCenter = false,
 		assignRoles = () => {
 			let _roles = _.clone(roles);
 
 			for (let seat in game.seated) {
-				let roleIndex = Math.floor((Math.random() * _roles.length));
+				let roleIndex = Math.floor((Math.random() * _roles.length)),
+					role = _roles[roleIndex];
 
-				game.internals[seat].trueRole = _roles[roleIndex];
+				if (role === 'werewolf' && !allWerewolvesNotInCenter) {
+					allWerewolvesNotInCenter = true;
+				}
+
+				game.internals[seat].trueRole = role;
 				_roles.splice(roleIndex, 1);
 			}
 
 			_roles.forEach((role) => {  // spread op here?
 				game.internals.centerRoles.push(role);
 			});
-		},
-		werewolfRoleCount = roles.filter((role) => {
-			return role === 'werewolf';
-		}).length,
-		werewolvesInCenterCount;
+		};
 
 	assignRoles();
-	werewolvesInCenterCount = game.internals.centerRoles.filter((role) => {
-		return role === 'werewolf';
-	}).length;
 
-	if (game.kobk && werewolvesInCenterCount === werewolfRoleCount) {
+	if (game.kobk && !allWerewolvesNotInCenter) {
 		// need some sort of do while loop here
 	}
 	console.log(game.internals);
