be2a668302363d061693bf85bc2bee7469379cde
Chris Ozols <chris.v.ozols@gmail.com>
2016-01-02 22:08:36 -0600

    updates to nightactionevent, still needs a lot of work

--- routes/socket/game-nightactions.js
+++ routes/socket/game-nightactions.js
@@ -6,8 +6,26 @@ import _ from 'lodash';
 
 export function updateUserNightActionEvent(socket, data) {
 	let game = games.find((el) => {
-		return el.uid === data.uid;
-	});
+			return el.uid === data.uid;
+		}),
+		eventMap = {
+			singleWerewolf() {
+				let roleClicked = getTrueRoleBySeatNumber(data.action);
 
+				// todo: update tableState object for this socket and emit gameupdate then update table.jsx componentdidupdate to add the role's class to the clicked card and then revealcard the clicked card and then emit a gamechat with that info.
+			}
+		}
 	console.log(data);
+	console.log(game.internals.seatedPlayers);
+	eventMap[data.role]();
+}
+
+let getTrueRoleBySeatNumber = (game, num) => {
+	num = parseInt(num);
+
+	if (num < 8) {
+		return game.internals.seatedPlayers[num - 1].trueRole;
+	} else {
+		return game.internals.centerRoles[num - 8];
+	}
 }
