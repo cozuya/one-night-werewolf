39f07103bbbc52b61b7b1cd2df2cbb7918b1fa30
Chris Ozols <chris.v.ozols@gmail.com>
2016-02-07 17:25:59 -0600

    util updates

--- routes/socket/util.js
+++ routes/socket/util.js
@@ -3,6 +3,44 @@
 import { games } from './game.js';
 import _  from 'lodash';
 
+
+// prod
+// export let devStatus = { // can't think of a better name.  This object assists in development.
+// 	nightPhasePause: 5,
+// 	phaseTime: 10,
+// 	endingGame: 15,
+// 	seatedCountToStartGame: 7,
+// 	startGamePause: 5,
+//	playerCountToEndGame: 7,
+//	revealWinnersPause: 6000,
+//	revealAllCardsPause: 6000
+// }
+
+// dev 2p
+// export let devStatus = {
+// 	nightPhasePause: 1,
+// 	phaseTime: 1,
+// 	endingGame: 3,
+// 	seatedCountToStartGame: 2,
+// 	startGamePause: 1,
+// 	playerCountToEndGame: 2,
+// 	revealWinnersPause: 1000,
+// 	revealAllCardsPause: 1500
+// }
+
+// dev 7p
+
+export let devStatus = {
+	nightPhasePause: 1,
+	phaseTime: 1,
+	endingGame: 3,
+	seatedCountToStartGame: 7,
+	startGamePause: 1,
+	playerCountToEndGame: 7,
+	revealWinnersPause: 1000,
+	revealAllCardsPause: 1500
+}
+
 export function getSocketsByUid(uid) {
 	let game = games.find((el) => {
 			return el.uid === uid;
@@ -36,28 +74,4 @@ export function getInternalPlayerInGameByUserName(game, userName) {
 	return game.internals.seatedPlayers.find((player) => {
 		return player.userName === userName;
 	});
-}
-
-// prod
-// export let devStatus = { // can't think of a better name.  This object assists in development.
-// 	nightPhasePause: 5,
-// 	phaseTime: 10,
-// 	endingGame: 15,
-// 	seatedCountToStartGame: 7,
-// 	startGamePause: 5,
-//	playerCountToEndGame: 7,
-//	revealWinnersPause: 6000,
-//	revealAllCardsPause: 6000
-// }
-
-// dev
-export let devStatus = {
-	nightPhasePause: 1,
-	phaseTime: 1,
-	endingGame: 3,
-	seatedCountToStartGame: 2,
-	startGamePause: 1,
-	playerCountToEndGame: 2,
-	revealWinnersPause: 1000,
-	revealAllCardsPause: 1500
 }
